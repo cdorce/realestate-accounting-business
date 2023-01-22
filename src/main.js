614f78c3380947304767a76eeeed321adb1af5e7
Cleffton Dorce <67771337+cdorce@users.noreply.github.com>
2023-01-22 15:42:05 -0800

    navbar-done-2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar__menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let openBtn = document.querySelector(".menu-open");
 let closeBtn = document.querySelector(".menu-close");
 let menu = document.querySelector(".navbar__menu"); 
@@ -15,4 +46,4 @@ closeBtn.addEventListener("click", ()=>{
     openBtn.classList.toggle("hide");
     closeBtn.classList.toggle("hide");
     menu.classList.toggle("hidden");
-});
+}); */
